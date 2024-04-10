5015ca2f7cffff4098294e931f3311243d9fff85
Dak-Ore <dakore879@gmail.com>
2024-04-10 02:14:52 +0200

    Update

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: dak <dak@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:14:42 by dak               #+#    #+#             */
-/*   Updated: 2024/04/09 18:21:04 by dak              ###   ########.fr       */
+/*   Updated: 2024/04/10 02:14:03 by dak              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n)
+	while (src[i] && i < n)
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,20 +6,26 @@
 /*   By: dak <dak@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:51:45 by dak               #+#    #+#             */
-/*   Updated: 2024/04/08 18:41:39 by dak              ###   ########.fr       */
+/*   Updated: 2024/04/10 02:08:17 by dak              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	src_size;
 
 	i = 0;
+	src_size = 0;
+	while (src[src_size])
+		src_size++;
+	if (size == 0)
+		return (src_size);
 	while (i < size - 1 && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (i);
+	return (src_size);
 }
